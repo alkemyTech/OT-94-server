d29dfd8f6eeee2b5569efc196807b731c261b772
Meli Yanguas <melinayanguas@gmail.com>
2021-10-28 00:27:51 -0300

    title component added

---
+++ src/Components/Titles/Titles.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Titles.css';
+
+const Titles = (props) => {
+    console.log(props.imagenTitulo)
+    return (
+        <div className="titulo-contenedor">
+            <h1>{props.titulo}</h1>
+            <img src={props.imagenTitulo ? props.imagenTitulo : "/images/latest-02.jpg"}/>
+            
+        </div>
+    )
+}
+
+export default Titles
