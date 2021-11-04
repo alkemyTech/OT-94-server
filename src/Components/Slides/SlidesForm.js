f8b450e450d36cbd4c5379bf3472a84a8dee33d5
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-03 23:31:12 -0300

    Progress in slide creation/edition component

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,4 +1,139 @@
-import React, { useState } from 'react';
+import React from 'react';
+import { Formik, Form, Field, ErrorMessage } from 'formik';
+
+import '../FormStyles.css';
+
+export default function ContactForm() {
+
+    return(
+        <div>
+            <h1>Contacto</h1>
+            <Formik
+                initialValues={{
+                    name: '',
+                    description: '',
+                    order: '',
+                    image: ''
+                }}
+
+                validate={(values) => {
+                    let errors = {}
+
+                    if(!values.name){
+                        errors.name = 'Ingrese su nombre'
+                    }else if(values.name.length < 4) {
+                        errors.name = 'El nombre debe contener 4 caracteres como mínimo'
+                    }
+
+                    if(!values.description){
+                        errors.description = 'Ingrese una descripción'
+                    }
+
+                    if(!values.order){
+                        errors.order = 'Ingrese un numero de orden'
+                    }
+
+                    if(!values.image){
+                        errors.image = 'Ingrese una imagen'
+                    }else if(!values.image.name.includes('.JPG') || !values.image.name.includes('.jpg') || !values.image.name.includes('.PNG') || !values.image.name.includes('.png')){
+                        errors.image = 'Imagen con formato inválido'
+                    }
+
+                    return errors;
+                }}
+
+
+                onSubmit={(values) => {
+                    console.log({ 
+                        fileName: values.image.name, 
+                        type: values.image.type,
+                        size: `${values.image.size} bytes`
+                      })
+                }}
+
+            >
+                {( {errors, setFieldValue} ) => (
+                    <Form className="form-container">
+                        <div>
+                            <label htmlFor="name">Nombre</label>
+                            <Field 
+                                className="input-field"
+                                type="text"
+                                id="name"
+                                name="name"
+                                placeholder="Ingrese su nombre"
+                            />
+                            <ErrorMessage name="name" component={() => (<div>{errors.name}</div>)} />
+                        </div>
+                        <div>
+                            <label htmlFor="description">Descripción</label>
+                            <Field 
+                                className="input-field"
+                                type="text"
+                                id="description"
+                                name="description"
+                                placeholder="Ingrese una descripción"
+                            />
+                            <ErrorMessage name="description" component={() => (<div>{errors.description}</div>)} />
+                        </div>
+                        <div>
+                            <label htmlFor="order">Orden</label>
+                            <Field 
+                                className="input-field"
+                                type="text"
+                                id="order"
+                                name="order"
+                                placeholder="Ingrese el numero de orden"
+                            />
+                            <ErrorMessage name="order" component={() => (<div>{errors.order}</div>)} />
+                        </div>
+                        <div>
+                            <label htmlFor="image">Imagen</label>
+                            <input id="image" name="image" type="file" onChange={(event) => {
+                                setFieldValue("image", event.currentTarget.files[0]);
+                            }} />
+                            <ErrorMessage name="image" component={() => (<div>{errors.image}</div>)} />
+                        </div>
+                        <button className="submit-btn" type="submit">Enviar</button>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+        
+    )
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import React, { useState } from 'react';
 import '../FormStyles.css';
 
 const SlidesForm = () => {
@@ -29,4 +164,4 @@ const SlidesForm = () => {
     );
 }
  
-export default SlidesForm;
+export default SlidesForm; */
