2cda6ff7031d281c7d4edec57d67526e0534f072
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-04 16:31:40 -0300

    First version of slide form ready

--- src/Components/Slides/SlidesForm.js
+++ src/Components/Slides/SlidesForm.js
@@ -1,22 +1,14 @@
-import React, {useRef} from 'react';
+import React from 'react';
 import { Formik, Form, Field, ErrorMessage } from 'formik';
+import axios from 'axios';
 import { CKEditor } from '@ckeditor/ckeditor5-react';
 import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
-import imagen from '../Slides/imagenPrueba.jpg'
 
 import '../FormStyles.css';
 
 export default function ContactForm() {
 
-    const buttonRef = useRef()
-
-    const data = [
-        'nombre',
-        'esta es una descripción',
-        1,
-        imagen
-
-    ]
+    let objectRecieved = false;
 
     return(
         <div>
@@ -56,13 +48,32 @@ export default function ContactForm() {
 
 
                 onSubmit={(values) => {
-                    console.log(buttonRef.current)
                     console.log(values)
                     console.log({ 
                         fileName: values.image.name, 
                         type: values.image.type,
                         size: `${values.image.size} bytes`
-                      })
+                    })
+
+                    /*Al ser requisito que cada numero de orden sea único, interpreto que el numero de orden se corresponde con el id*/
+
+                    if(objectRecieved === true) {
+                        axios.patch(`/slides/${values.order}`, values)
+                            .then(function (response) {
+                                console.log(response);
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                    }else {
+                        axios.post('/slides/create', values)
+                            .then(function (response) {
+                                console.log(response);
+                            })
+                            .catch(function (error) {
+                                console.log(error);
+                            });
+                    }
                 }}
 
             >
@@ -104,23 +115,11 @@ export default function ContactForm() {
                         </div>
                         <div>
                             <label htmlFor="image">Imagen</label>
-                            <input ref={buttonRef} id="image" name="image" type="file" onChange={(event) => {
-                                event.currentTarget.files[0] = imagen
+                            <input id="image" name="image" type="file" onChange={(event) => {
                                 setFieldValue("image", event.currentTarget.files[0]);
                             }} />
                             <ErrorMessage name="image" component={() => (<div>{errors.image}</div>)} />
                         </div>
-                        <button 
-                            type="button"
-                            className="submit-btn"
-                            onClick={() => {
-                                setFieldValue("name", data[0])
-                                setFieldValue("description", data[1])
-                                setFieldValue("order", data[2])
-                                
-                            }}
-                        >Editar ya existente
-                        </button>
                         <button className="submit-btn" type="submit">Enviar</button>
                     </Form>
                 )}
@@ -129,66 +128,3 @@ export default function ContactForm() {
         
     )
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/* import React, { useState } from 'react';
-import '../FormStyles.css';
-
-const SlidesForm = () => {
-    const [initialValues, setInitialValues] = useState({
-        name: '',
-        description: ''
-    });
-
-    const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value})
-        } if(e.target.name === 'description'){
-            setInitialValues({...initialValues, description: e.target.value})
-        }
-    }
-
-    const handleSubmit = (e) =>{
-        e.preventDefault();
-        console.log(initialValues);
-    }
-
-    return (
-        <form className="form-container" onSubmit={handleSubmit}>
-            <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Slide Title"></input>
-            <input className="input-field" type="text" name="description" value={initialValues.description} onChange={handleChange} placeholder="Write the description"></input>
-            <button className="submit-btn" type="submit">Send</button>
-        </form>
-    );
-}
- 
-export default SlidesForm; */
