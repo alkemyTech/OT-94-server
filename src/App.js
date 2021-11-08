a98ff100da0c8197152d523015fcebe196462570
Meli Yanguas <melinayanguas@gmail.com>
2021-11-08 12:41:17 -0300

    home page created

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import ToysCampaign from './Campaigns/Toys/ToysCampaign';
 import MembersForm from './Components/Members/MembersForm';
 import ProjectsForm from './Components/Projects/ProjectsForm';
 import ContactPage from './Components/Contact/ContactPage';
+import Home from './Components/home';
 
 
 function App() {
@@ -33,6 +34,7 @@ function App() {
           <Route path="/school-campaign" component={SchoolCampaign} />
           <Route path="/toys-campaign" component={ToysCampaign} />
           <Route path="/contacto" component={ContactPage} />
+          <Route exact path="/" component={Home} />
         </Switch>
       </BrowserRouter>
     <div className="App">
---
+++ src/Components/home/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Home = () => {
+    return (
+        <div>
+            pagina de inicio works .... 
+        </div>
+    )
+}
+
+export default Home
