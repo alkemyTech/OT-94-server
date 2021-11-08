7ac0f5741d2e641c046b4d8b4797107205e5d056
Meli Yanguas <melinayanguas@gmail.com>
2021-11-08 14:07:42 -0300

    sprint 2

--- src/Components/home/index.js
+++ src/Components/home/index.js
@@ -1,21 +1,12 @@
-import React, { useEffect, useState } from 'react'
+import React from 'react'
 
 const Home = () => {
 
-    // const [dataONG, setDataONG] = useState([]);
-
-    // useEffect(() => {
-    //     const getDataONG = () => {
-    //       fetch('https://example')
-    //         .then((res) => res.json())
-    //         .then((res) => setDataONG(res));
-    //     };
-    //     getDataONG();
-    //   }, []);
+    const welcomeText = 'texto de bienvenida example'
 
     return (
         <div>
-            <h1>welcome text</h1>
+            <h1>{welcomeText}</h1>
             <div>//placeholder
             //slider  //listado de novedades
             </div>
