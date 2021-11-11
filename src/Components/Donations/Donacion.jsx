3613809ea32f0175713b8a4dd4f09f2f59c170a2
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-11 20:48:37 -0300

    MercadoPago link erased

--- src/Components/Donations/Donacion.jsx
+++ src/Components/Donations/Donacion.jsx
@@ -5,7 +5,6 @@ export default function Donacion ( {texto} ) {
     return (
         <div>
             {<p>{texto ? texto : "Texto dinámico"}</p>}
-            <a href="https://mpago.la/32ikPWE">Pagar</a>
         </div>
     )
 }
