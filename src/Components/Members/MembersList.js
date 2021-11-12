8cd4a73622cbfa21293e826568abe617feda84d6
Meli Yanguas <melinayanguas@gmail.com>
2021-11-12 19:19:45 -0300

    memberslist stylized

--- src/Components/Members/MembersList.js
+++ src/Components/Members/MembersList.js
@@ -1,18 +1,20 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import MockList from './membersMock.json' //fotos sacas de random user generator
+import './MemberList.css'
 
 const MembersList = () => {
     return (
-        <div>
+        <div className="members-list">
             <Link to='/backoffice/members/create'>link a....crear miembro</Link>
-            <h2>miembros de la ONG </h2>
+            <h2>Miembros de la ONG </h2>
 
             <table>
                 <thead>
                     <tr>
                         <th>name</th>
                         <th>photo</th>
+                        <th>actions</th>
                     </tr>
                 </thead>
                 <tbody>
