68d56ff94bab22e5cc7acd5e675d72c936a76bec
Meli Yanguas <melinayanguas@gmail.com>
2021-11-18 15:02:05 -0300

    create env for activities

--- src/Components/Activities/ActivitiesForm.js
+++ src/Components/Activities/ActivitiesForm.js
@@ -5,6 +5,9 @@ import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import '../FormStyles.css';
 
 const ActivitiesForm = () => {
+
+    const { REACT_APP_URL_ACTIVITIES } = process.env
+
     const [initialValues, setInitialValues] = useState({
         name: '',
         description: '',
@@ -30,24 +33,24 @@ const ActivitiesForm = () => {
     }, [])
 
     const handleChange = (e) => {
-        if(e.target.name === 'name'){
-            setInitialValues({...initialValues, name: e.target.value});
+        if (e.target.name === 'name') {
+            setInitialValues({ ...initialValues, name: e.target.value });
             VALIDATION.name.test(e.target.value) ? setErrorName(false) : setErrorName(true);
         } else {
-            setInitialValues({ ...initialValues, image: e.target.value});
+            setInitialValues({ ...initialValues, image: e.target.value });
             VALIDATION.image.test(e.target.value) ? setErrorImage(false) : setErrorImage(true);
         }
     }
 
-    const handleSubmit = (e) => {
+    const handleSubmit = (e, id) => {
         e.preventDefault();
         const fetchData = async (values) => {
             try {
                 if (petition) {
-                    const { data } = await axios.post('/activities/create', values);
+                    const { data } = await axios.post(`${REACT_APP_URL_ACTIVITIES}/create`, values);
                     console.log(data)
                 } else {
-                    const { data } = await axios.patch(`/activities/:id`, values);
+                    const { data } = await axios.patch(`${REACT_APP_URL_ACTIVITIES}/${id}`, values);
                     console.log(data)
                 }
             } catch (error) {
@@ -61,19 +64,19 @@ const ActivitiesForm = () => {
             setTimeout(() => setErrorSendData(false), 2000)
         }
     }
-    
+
     return (
         <form className="form-container" onSubmit={handleSubmit}>
             <input className="input-field" type="text" name="name" value={initialValues.name} onChange={handleChange} placeholder="Activity Title"></input>
             {errorName ? <p>Error, it must only contain letters and a minium length of 3 characters</p> : null}
             <CKEditor className="input-field" name="description"
-                    editor={ ClassicEditor }
-                    data={initialValues.description}
-                    onChange={ ( event, editor ) => {
-                        const data = editor.getData();
-                        setInitialValues({ ...initialValues, description: data});
-                    } }
-                />
+                editor={ClassicEditor}
+                data={initialValues.description}
+                onChange={(event, editor) => {
+                    const data = editor.getData();
+                    setInitialValues({ ...initialValues, description: data });
+                }}
+            />
             <input type="file" name="image" onChange={handleChange} />
             {errorImage ? <p>Error, image must be in .jpg or .png format</p> : null}
             <button className="submit-btn" type="submit">Send</button>
@@ -81,5 +84,5 @@ const ActivitiesForm = () => {
         </form>
     );
 }
- 
+
 export default ActivitiesForm;
