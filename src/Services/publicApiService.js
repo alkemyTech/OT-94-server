9e5c9b64964716656ba88ba3257e626d08c02225
Meli Yanguas <melinayanguas@gmail.com>
2021-11-18 08:39:14 -0300

    publicApiService modifed

--- src/Services/publicApiService.js
+++ src/Services/publicApiService.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 
 const config = {
     headers: {
-        Group: 01                //Aqui va el ID del equipo!!
+        //Aqui va el ID del equipo!!
     }
 }
 
-const Get = () => {
-    axios.get('https://jsonplaceholder.typicode.com/users', config)
-    .then(res => console.log(res))
-    .catch(err => console.log(err))
+const route = 'https://jsonplaceholder.typicode.com/users';
+const id = 1;
+
+const Get = (route, id) => {
+    axios.get(`${route}/${id}`, config)
+        .then(res => console.log(res))
+        .catch(err => console.log(err))
 }
 
 export default Get
