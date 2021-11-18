08c958fcff9f6c596ccdb0d83085cd5ef4d83779
Emiliano <emilianogorgellon49@gmail.com>
2021-11-18 03:21:23 -0300

    Component Layout include components Header/Footer from folder School

---
+++ src/Components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Header from '../../Campaigns/School/Header';
+import Footer from '../../Campaigns/School/Footer';
+const Layout = () => {
+    return (
+        <div>
+            <Header props="props" />
+            <Footer props="props" />
+        </div>
+    )
+}
+
+export default Layout;
