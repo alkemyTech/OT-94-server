e17e6374167b653fa8adda47375854bcd2958215
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-20 17:06:10 -0300

    Alert and Loader added to home/index.js and About/AboutPage.js.Data petition was also created in both components

--- src/Components/About/AboutPage.js
+++ src/Components/About/AboutPage.js
@@ -1,14 +1,42 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import axios from 'axios'
 import Titles from '../Titles/Titles'
+import Alert from '../Skeleton/Alert'
+import Loader from '../Skeleton/Loader'
 
 const AboutPage = () => {
 
     const SobreNosotrosText = 'Texto sobre nosotros example'
 
+    const [texto, setTexto] = useState("cargando")
+    const [huboError, setHuboError] = useState(false)
+
+    useEffect(() => {
+
+        async function getText() {
+            try {
+                const texto = await axios.get("url")
+                setTexto(texto.data)
+            } catch (err) {
+                console.log(err)
+                setHuboError(true)
+                setTexto(SobreNosotrosText)
+            }
+        }
+
+        getText()
+
+    }, [])
+
     return (
         <div>
             <Titles titulo='Nosotros' imagenTitulo='https://a.slack-edge.com/ddb1dac/marketing/img/careers/img-hero-slack-careers.jpg' />
-            <p>{SobreNosotrosText}</p>
+            {
+                texto === "cargando" ? <Loader /> : <p>{texto}</p>
+            }
+            {
+                huboError && <Alert />
+            }
         </div>
     )
 }
