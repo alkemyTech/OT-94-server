e5e8b68f579d9fb9262c601b9550fa522b702dd5
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-11-22 01:09:48 -0300

    added Private and Public Path

---
+++ src/Components/Routes/AppPrivateRoutes.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { Switch, Route, Redirect } from "react-router-dom";
+import ScreenDashboard from "../Backoffice/ScreenDashboard";
+import HomeFormEdit from "../home/HomeFormEdit";
+import NewsForm from "../News/NewsForm";
+import UsersFormCreateEdit from '../Users/UsersFormCreateEdit';
+import SlidesForm from '../Slides/SlidesForm';
+import ProjectsFormCreateEdit from "../Projects/ProjectsFormCreateEdit";
+import MembersList from "../Members/MembersList";
+import MemberFormCreateEdit from "../Members/MemberFormCreateEdit";
+import Organization from "../Organization/Organization";
+import OrganizationForm from "../Organization/OrganizationForm";
+
+
+export const AppPrivateRoutes = () => {
+  return (
+      <Switch>
+          
+          <Route path="/backoffice" exact component={ScreenDashboard}/>
+          <Route path="/backoffice/home" exact component={HomeFormEdit} />
+          <Route path="/backoffice/news" exact component={NewsForm} />
+          <Route path="/backoffice/create-user" exact component={UsersFormCreateEdit}/>
+          <Route path="/backoffice/create-slide" exact component={SlidesForm} />
+          <Route path="/backoffice/create-project" exact component={ProjectsFormCreateEdit}/>
+          <Route path="/backoffice/members" exact component={MembersList}/>
+          <Route path="/backoffice/members/edit" exact component={MemberFormCreateEdit}/>
+          <Route path="/backoffice/organization" exact component={Organization} />
+          <Route path="/backoffice/organization/edit" exact component={OrganizationForm}/>
+
+         <Redirect to="/" />
+    </Switch>
+  );
+};
---
+++ src/Components/Routes/AppPublicRoutes.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import SchoolCampaign from '../../Campaigns/School/SchoolCampaign';
+import ToysCampaign from '../../Campaigns/Toys/ToysCampaign';
+import AboutPage from '../About/AboutPage';
+import ActivitiesForm from '../Activities/ActivitiesForm';
+import ActivitiesList from '../Activities/ActivitiesList';
+import ActivitiesDetail from '../Activities/Detail/ActivitiesDetail';
+import CategoriesForm from '../Categories/CategoriesForm';
+import ContactPage from '../Contact/ContactPage';
+import Donacion from '../Donations/Donacion';
+import Gracias from '../Donations/Gracias';
+import Home from '../home';
+import MembersForm from '../Members/MembersForm';
+import NewsDetail from '../News/Detail/NewsDetail';
+import NewsList from '../News/NewsList';
+import ProjectsForm from '../Projects/ProjectsForm';
+import TestimonialForm from '../Testimonials/TestimonialsForm';
+import UserForm from '../Users/UsersForm';
+
+export const AppPublicRoutes = () => {
+  const newsMock = [
+    { id: 2, name: 'Titulo de prueba', description: 'Descripcion de prueba' },
+    { id: 1, name: 'Titulo de prueba', description: 'Descripcion de prueba' },
+    { id: 3, name: 'Titulo de prueba', description: 'Descripcion de prueba' },
+  ];
+
+  return (
+    <>
+      <Switch>
+        <Route path='/' exact component={Home} />
+        <Route path='/donar' exact component={Donacion} />
+         <Route path='/gracias' component={Gracias} />
+        <Route path='/contacto' component={ContactPage} />
+        <Route path='/nosotros' component={AboutPage} />
+        <Route path='/create-user' exact component={UserForm} />
+        <Route path='/school-campaign' exact component={SchoolCampaign} />
+        <Route path='/toys-campaign' exact component={ToysCampaign} />
+        <Route path='/create-member' exact component={MembersForm} />
+        <Route path='/create-project' exact component={ProjectsForm} />
+        <Route path='/create-activity' exact component={ActivitiesForm} />
+        <Route path='/create-category' exact component={CategoriesForm} />
+        <Route path='/create-testimonials' exact component={TestimonialForm} />-
+        <Route path='/novedades' exact>
+          <NewsList novedades={newsMock} />
+        </Route>
+        <Route path='/novedades/:id' component={NewsDetail} />
+        <Route path='/actividades' exact component={ActivitiesList} />
+        <Route path='/actividades/:id' component={ActivitiesDetail} />
+        
+        <Redirect to='/backoffice' />
+      </Switch>
+    </>
+  );
+};
