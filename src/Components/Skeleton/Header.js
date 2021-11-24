b1165958cbf20807e21699e6466455a5f68f025e
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-11-24 16:49:45 -0300

    Show Active Link

--- src/Components/Skeleton/Header.js
+++ src/Components/Skeleton/Header.js
@@ -1,26 +1,31 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FaBars } from 'react-icons/fa';
 
 // https://www.geeksforgeeks.org/create-a-responsive-navbar-using-reactjs/
 const Header = ({ menu }) => {
-	console.log(menu);
-	const menuMap = menu.map((element) => {
-		return (
-			<Link to={element.link} activeStyle className="NavLink ">
-				{element.menu}
-			</Link>
-		);
-	});
-	return (
-		<>
-			<nav className="Nav">
-				<FaBars className="Bars" />
-				<div className="NavMenu">{menuMap}</div>
-			</nav>
-		</>
-	);
+  console.log(menu);
+  const menuMap = menu.map((element) => {
+    return (
+      <NavLink
+        to={element.link}
+        activeStyle={{ color: 'black' }}
+        exact
+        className='NavLink '
+      >
+        {element.menu}
+      </NavLink>
+    );
+  });
+  return (
+    <>
+      <nav className='Nav'>
+        <FaBars className='Bars' />
+        <div className='NavMenu'>{menuMap}</div>
+      </nav>
+    </>
+  );
 };
 
 export default Header;
