fd7efb5d85a9b124279c9a1b8e1901f29e27ab5d
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-25 15:52:02 -0300

    Leaflet map integration. Use of state pending

--- src/Components/Contact/LeafletMap.jsx
+++ src/Components/Contact/LeafletMap.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import '../Contact/LeafletMap.css'
+import { Get } from '../../Services/privateApiService'
+import Alert from '../Skeleton/Alert'
 
 export default function LeafletMap() {
+
+    const [direccion, setDireccion] = useState()
+    const [huboError, setHuboError] = useState(false)
+
     return (
-        <div id="map">
-            <MapContainer center={[51.505, -0.09]} zoom={13} scrollWheelZoom={false}>
+        <div className="contenedor">
+            <MapContainer center={[51.500, -0.09]} zoom={13} scrollWheelZoom={true}>
                 <TileLayer
                     attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
                     url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
                 />
-                <Marker position={[51.505, -0.09]}>
+                <Marker position={[51.500, -0.09]}>
                     <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
+                    Sede de Somos Mas
                     </Popup>
                 </Marker>
             </MapContainer>
+            {huboError && 
+                <Alert 
+                    showAlert = {true}
+                    title = "Error"
+                    text = "Hubo un error cargando la dirección en el mapa"
+                    type = "error"
+                    cancelButton = {false}
+                    confirmButtonText = "OK"
+                    cancelButtonText = "Cancel"
+                    showDenyButton = {true}
+                />
+            }
         </div>
     )
 }
