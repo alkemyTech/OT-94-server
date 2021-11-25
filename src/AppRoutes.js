6dc46d7a8d924f5c28a514599974e6bc146b2130
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-11-25 10:57:27 -0300

    adding protection to private routes with admin role

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -1,24 +1,33 @@
-import React from "react";
+import React from 'react';
 import './App.css';
-import { BrowserRouter, Switch } from "react-router-dom";
-import { PrivateRoute } from "./Components/Routes/PrivateRoute";
-import { PublicRoute } from "./Components/Routes/PublicRoute";
-import { AppPrivateRoutes } from "./Components/Routes/AppPrivateRoutes";
-import { AppPublicRoutes } from "./Components/Routes/AppPublicRoutes";
+import { BrowserRouter, Switch } from 'react-router-dom';
+import { PrivateRoute } from './Components/Routes/PrivateRoute';
+import { PublicRoute } from './Components/Routes/PublicRoute';
+import { AppPrivateRoutes } from './Components/Routes/AppPrivateRoutes';
+import { AppPublicRoutes } from './Components/Routes/AppPublicRoutes';
 
 export const AppRoutes = () => {
+  // realizar llamado a Redux de autenticación creado en tkt OT94-106
+  const user = { logged: true, role: 'administrador' };
 
-   // realizar llamado a Redux de autenticación creado en tkt OT94-106
-    const user = { 'logged': false };
-  
-    return (
-        <BrowserRouter>
-            <div>
-              <Switch>
-                <PrivateRoute path="/backoffice" component={AppPrivateRoutes } isAuthenticated={user.logged} />
-                <PublicRoute  path="/" component={AppPublicRoutes}  isAuthenticated={user.logged} /> 
-              </Switch>
-            </div>
-        </BrowserRouter>
-    )
-}
+  return (
+    <BrowserRouter>
+      <div>
+        <Switch>
+          <PrivateRoute
+            path='/backoffice'
+            component={AppPrivateRoutes}
+            isAuthenticated={user.logged}
+            role={user.role}
+          />
+          <PublicRoute
+            path='/'
+            component={AppPublicRoutes}
+            isAuthenticated={user.logged}
+            role={user.role}
+          />
+        </Switch>
+      </div>
+    </BrowserRouter>
+  );
+};
