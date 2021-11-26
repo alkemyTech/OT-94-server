92122f7a04c18e237248ffe546b3e5dafb695d21
Emiliano <emilianogorgellon49@gmail.com>
2021-11-26 04:15:55 -0300

    Modify store and create activityReducer with thunk

--- src/app/store.js
+++ src/app/store.js
@@ -2,10 +2,13 @@ import { configureStore } from '@reduxjs/toolkit';
 import counterReducer from '../features/counter/counterSlice';
 import authReducer from '../features/user/userSlice';
 import alertReducer from "../features/alert/alertSlice";
+import activityReducer from "../features/activity/activitySlice";
+
 export default configureStore({
   reducer: {
     counter: counterReducer,
     user: authReducer,
-    alert: alertReducer
+    alert: alertReducer,
+    listActivity: activityReducer
   },
 });
