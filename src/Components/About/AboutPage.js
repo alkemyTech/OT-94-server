841151ea8ffa68c67506c01b0b96d29f16f03062
Emiliano <emilianogorgellon49@gmail.com>
2021-11-27 20:04:52 -0300

    Add style in /nosotros

--- src/Components/About/AboutPage.js
+++ src/Components/About/AboutPage.js
@@ -1,49 +1,34 @@
 import React, {useEffect, useState} from 'react'
 import axios from 'axios'
-import Titles from '../Titles/Titles'
-import Alert from '../Skeleton/Alert'
-import Loader from '../Skeleton/Loader'
 import MemberList from './MemberList'
 
 const AboutPage = () => {
 
     const aboutMock = [
-        {name: 'Luis', image: "https://image.shutterstock.com/image-vector/profile-anonymous-face-icon-gray-260nw-789318310.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"},
-        {name: 'Sandra', image: "https://image.shutterstock.com/image-vector/profile-anonymous-face-icon-gray-260nw-789318310.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"},
-        {name: 'Javier', image: "https://image.shutterstock.com/image-vector/profile-anonymous-face-icon-gray-260nw-789318310.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"}
+        {name: 'Brad', image: "https://randomuser.me/api/portraits/men/75.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"},
+        {name: 'Jessica', image: "https://randomuser.me/api/portraits/women/76.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"},
+        {name: 'Lisandro', image: "https://randomuser.me/api/portraits/men/77.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"},
+        {name: 'Rebeca', image: "https://randomuser.me/api/portraits/women/78.jpg", description: 'Descripcion de prueba', facebookUrl: "https://www.facebook.com", linkedinUrl: "https://www.linkedin.com"}
     ];
-
-    const SobreNosotrosText = 'Texto sobre nosotros example'
-
-    const [texto, setTexto] = useState("cargando")
-    const [huboError, setHuboError] = useState(false)
 
     useEffect(() => {
 
         async function getText() {
             try {
                 const texto = await axios.get("url")
-                setTexto(texto.data)
             } catch (err) {
                 console.log(err)
-                setHuboError(true)
-                setTexto(SobreNosotrosText)
             }
         }
-
         getText()
-
     }, [])
 
     return (
         <div>
-            <Titles titulo='Nosotros' imagenTitulo='https://a.slack-edge.com/ddb1dac/marketing/img/careers/img-hero-slack-careers.jpg' />
-            {
-                texto === "cargando" ? <Loader /> : <p>{texto}</p>
-            }
-            {
-                huboError && <Alert />
-            }
+            <div className="container--us">
+                <h1>Nosotros</h1>
+                <img className="img-us" src="https://a.slack-edge.com/ddb1dac/marketing/img/careers/img-hero-slack-careers.jpg" alt="img-us" />
+            </div>
             <MemberList miembros={aboutMock}/>
         </div>
     )
--- src/Components/About/MemberList.jsx
+++ src/Components/About/MemberList.jsx
@@ -1,21 +1,24 @@
 import React from 'react'
-
+import "./memberList.css";
+import { SiFacebook, SiLinkedin } from "react-icons/si"
 export default function MemberList({miembros}) {
 
     if(miembros){
         return (
             <div>
-                <ul>
+                <ul className="container--members">
                     {
                         miembros.length > 0 ?
                             miembros.map((miembro) => {
                                 return(
-                                    <li key={miembro.name}>
-                                        <h3>{miembro.name}</h3>
-                                        <img src={miembro.image} alt={miembro.name} />
-                                        <p>{miembro.description}</p>
-                                        <a href={miembro.facebookUrl}>Facebook</a>
-                                        <a href={miembro.linkedinUrl}>Linkedin</a>
+                                    <li key={miembro.name} className="member">
+                                        <h3 className="member--name">{miembro.name}</h3>
+                                        <img className="member--img" src={miembro.image} alt={miembro.name} />
+                                        <p className="member--description">{miembro.description}</p>
+                                        <div className="member--container-social">
+                                        <a target="_blank" href={miembro.facebookUrl}><SiFacebook className="member--social" /></a>
+                                        <a target="_blank" href={miembro.linkedinUrl}><SiLinkedin className="member--social" /></a>
+                                        </div>
                                     </li>
                                 )
                             })
