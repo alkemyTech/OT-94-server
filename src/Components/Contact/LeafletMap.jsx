80dd7b5d49789c57177645e45eb1f6320f4b7f07
Renzo Garcia <jore_ma@yahoo.com.ar>
2021-11-27 17:26:26 -0300

    fix position lat lon map

--- src/Components/Contact/LeafletMap.jsx
+++ src/Components/Contact/LeafletMap.jsx
@@ -1,39 +1,37 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
-import '../Contact/LeafletMap.css'
-import { Get } from '../../Services/privateApiService'
-import Alert from '../Skeleton/Alert'
+import '../Contact/LeafletMap.css';
+import { Get } from '../../Services/privateApiService';
+import Alert from '../Skeleton/Alert';
 
 export default function LeafletMap() {
+  const [direccion, setDireccion] = useState();
+  const [huboError, setHuboError] = useState(false);
+  const position = [-34.6037, -58.3816];
 
-    const [direccion, setDireccion] = useState()
-    const [huboError, setHuboError] = useState(false)
-
-    return (
-        <div className="contenedor">
-            <MapContainer center={[51.500, -0.09]} zoom={13} scrollWheelZoom={false}>
-                <TileLayer
-                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-                    url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker position={[51.500, -0.09]}>
-                    <Popup>
-                    Sede de Somos Mas
-                    </Popup>
-                </Marker>
-            </MapContainer>
-            {huboError && 
-                <Alert 
-                    showAlert = {true}
-                    title = "Error"
-                    text = "Hubo un error cargando la dirección en el mapa"
-                    type = "error"
-                    cancelButton = {false}
-                    confirmButtonText = "OK"
-                    cancelButtonText = "Cancel"
-                    showDenyButton = {true}
-                />
-            }
-        </div>
-    )
+  return (
+    <div className='contenedor'>
+      <MapContainer center={position} zoom={12} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+        />
+        <Marker position={position}>
+          <Popup>Sede de Somos Mas</Popup>
+        </Marker>
+      </MapContainer>
+      {huboError && (
+        <Alert
+          showAlert={true}
+          title='Error'
+          text='Hubo un error cargando la dirección en el mapa'
+          type='error'
+          cancelButton={false}
+          confirmButtonText='OK'
+          cancelButtonText='Cancel'
+          showDenyButton={true}
+        />
+      )}
+    </div>
+  );
 }
