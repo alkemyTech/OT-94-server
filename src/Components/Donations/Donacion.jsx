5a83423b4a29a7467f3dc64162529d550a2ac484
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-28 20:46:03 -0300

    Styles in donations page

--- src/Components/Donations/Donacion.jsx
+++ src/Components/Donations/Donacion.jsx
@@ -1,22 +1,26 @@
 import React, {useEffect} from 'react'
+import './Donacion.css'
 
 export default function Donacion ( {texto, preferenceId} ) {
 
+    const thanks = "Te estaremos enormemente agradecidos por tu contribución! Te garantizamos que tu donación estará destinada a mejorar la vida de muchos chicos/as."
+
     useEffect(() => {
 
-          const script = document.createElement('script');
+        const script = document.createElement('script');
 
-          script.src = "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
-          script.setAttribute('data-preference-id', preferenceId);
-          script.setAttribute('data-source', "button");
+        script.src = "https://www.mercadopago.com.ar/integrations/v1/web-payment-checkout.js";
+        script.setAttribute('data-preference-id', preferenceId);
+        script.setAttribute('data-source', "button");
+        
+        const donate = document.getElementById("add-button");
+        donate.appendChild(script);
 
-          const donate = document.getElementById("add-button");
-          donate.appendChild(script);
     }, []);
 
     return (
-        <div id="add-button">
-            {<p>{texto ? texto : "Texto dinámico"}</p>}
+        <div id="add-button" className="button-container">
+            {<p className="agradecimiento">{texto ? texto : thanks}</p>}
         </div>
     )
 }
