19890dd50224a632603bc42595ec65fc1807b0f1
Matias de la Rosa Sterbanoff <matiasleonel.dev@outlook.com>
2021-11-27 21:56:34 -0300

    School campaign section modified

--- src/Campaigns/School/Footer.js
+++ src/Campaigns/School/Footer.js
@@ -1,67 +1,89 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import { Formik, Form, Field, ErrorMessage } from "formik";
+import './SchoolFooter.css'
 
 const Footer = () => {
+
+  const [formFooter, setFormFooter] = useState()
+
+  function set() {
+    setFormFooter(true)
+    localStorage.setItem("formu-footer", true)
+  }
+
+  useEffect(() => {
+    localStorage.getItem("formu-footer") ? setFormFooter(true) : setFormFooter(false)
+  }, [])
+
   const VALIDATION = {
     name: /^[a-zA-Z\s]{3,}$/,
     email: /^[0-9a-zA-Z\._-]+@[0-9a-zA-Z\._-]+\.[a-z\.]{2,6}$/,
-    message: /^$/
+    /* message: /^$/ */
   }
   return (
-    localStorage.getItem("formu-footer") ? null :
-      <div>
-        <Formik initialValues={{
-          name: "",
-          email: "",
-          message: ""
-        }}
-          validate={(values) => {
-            let errors = {};
-            if (!VALIDATION.name.test(values.name)) {
-              errors.name = "Error, it must only contains letters and a minimum length of 3 characters."
-            }
-            if (!VALIDATION.email.test(values.email)) {
-              errors.email = "Error, you must complete the email.";
-            }
-            if (VALIDATION.message.test(values.message)) {
-              errors.message = "Error, you need complete this field."
-            }
-            return errors;
+    formFooter ? 
+        <div className="text">
+              <p>Ya te subscribiste a nuestro newsletter! Muchas gracias!</p>
+        </div> 
+      :
+      <>
+        <div className="text">
+          <p>Complete este formulario para subscribirse a nuestro newsletter y recibir todas las novedades de la campaña escolar</p>
+        </div>
+        <div>
+          <Formik initialValues={{
+            name: "",
+            email: "",
+            /* message: "" */
           }}
-          onSubmit={(values) => {
-            const DATA = {
-              name: values.name,
-              email: values.email,
-              message: values.message
-            }
-            localStorage.setItem("formu-footer", true);
-            return DATA;
-          }}
-        >
-          {({ errors }) => (
-            <Form className="form-container">
-              <div>
-                <label id="labelName">Name: </label>
-                <Field className="input-field" id="labelName" name="name" type="text" placeholder="Your name" />
-                <ErrorMessage name="name" component={() => <p>{errors.name}</p>} />
-              </div>
-              <div>
-                <label id="labelEmail">Email: </label>
-                <Field className="input-field" id="labelEmail" name="email" type="email" placeholder="Your Email" />
-                <ErrorMessage name="email" component={() => <p>{errors.email}</p>} />
-              </div>
-              <div>
-                <label id="labelMessage">Message: </label>
-                <Field className="input-field" id="labelMessage" name="message" as="textarea" />
-                <ErrorMessage name="message" component={() => <p>{errors.message}</p>} />
-              </div>
-              <div>
-                <button className="submit-btn" type="submit">Send</button>
-              </div>
-            </Form>
-          )}
-        </Formik>
-      </div>
+            validate={(values) => {
+              let errors = {};
+              if (!VALIDATION.name.test(values.name)) {
+                errors.name = "Este campo debe contener solo letras, y un mínimo de 3 caracteres."
+              }
+              if (!VALIDATION.email.test(values.email)) {
+                errors.email = "Debe completar este campo con su email.";
+              }
+              /* if (VALIDATION.message.test(values.message)) {
+                errors.message = "Debe completar este campo."
+              } */
+              return errors;
+            }}
+            onSubmit={(values) => {
+              const DATA = {
+                name: values.name,
+                email: values.email,
+                /* message: values.message */
+              }
+              /* localStorage.setItem("formu-footer", true); */
+              return DATA;
+            }}
+          >
+            {({ errors }) => (
+              <Form className="form-container container">
+                <div>
+                  <label id="labelName" className="label">Nombre: </label>
+                  <Field className="input-field name-mail" id="labelName" name="name" type="text" placeholder="Su nombre" />
+                  <ErrorMessage name="name" component={() => <p className="error">{errors.name}</p>} />
+                </div>
+                <div>
+                  <label id="labelEmail" className="label">Email: </label>
+                  <Field className="input-field name-mail" id="labelEmail" name="email" type="email" placeholder="Su Email" />
+                  <ErrorMessage name="email" component={() => <p className="error">{errors.email}</p>} />
+                </div>
+                {/* <div>
+                  <label id="labelMessage" className="label">Mensaje: </label>
+                  <Field className="input-field message" id="labelMessage" name="message" as="textarea" />
+                  <ErrorMessage name="message" component={() => <p className="error">{errors.message}</p>} />
+                </div> */}
+                <div>
+                  <button className="submit-btn boton-submit" type="submit" onClick={set}>Enviar</button>
+                </div>
+              </Form>
+            )}
+          </Formik>
+        </div>
+      </>
   );
 }
 
